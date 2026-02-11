706bf2ae6790290cd2228988ae13ee9c2f19216e
nguyennamtuyen2k8-a11y <nguyennamtuyen2k8@gmail.com>
2026-02-11 09:12:52 +0700

    Update script.js

--- script.js
+++ script.js
@@ -4,3 +4,7 @@ function addToCart() {
     cart++;
     document.getElementById("cart").innerText = "Giỏ hàng: " + cart;
 }
+function dongThongBao() {
+  document.getElementById("popupThongBao").style.display = "none";
+}
+
